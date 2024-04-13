46b64f78cb3d297990cc084e8a996a72071bb0d8
NIKOLAEVTOLIA <140529562+NIKOLAEVTOLIA@users.noreply.github.com>
2024-04-13 21:11:29 +0300

    all done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+  private signature: string = "";
+
+  getSignature(): string {
+    if (!this.signature) {
+      this.signature = Math.random().toString(36).substring(7);
+    }
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} entered the house.`);
+    } else {
+      console.log("The door is closed. Cannot let anyone in.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Invalid key. Cannot open the door.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +53,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
